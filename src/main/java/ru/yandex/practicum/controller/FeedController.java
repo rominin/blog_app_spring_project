f3fceec56ae0e5c39035f80c34a8a68dd249e5dc
Roman Orinichev <roma-orinich@yandex.ru>
2025-01-22 19:51:42 +0100

    Added controller and view for general feed

--- src/main/java/ru/yandex/practicum/controller/FeedController.java
+++ src/main/java/ru/yandex/practicum/controller/FeedController.java
@@ -1,15 +1,34 @@
 package ru.yandex.practicum.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.yandex.practicum.model.Post;
+import ru.yandex.practicum.service.PostService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/feed")
 public class FeedController {
 
+    private final PostService postService;
+
+    public FeedController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping
-    public String feed() {
+    public String feed(
+            @RequestParam(name = "page", defaultValue = "1") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size,
+            Model model) {
+        List<Post> posts = postService.getPosts(page, size);
+        model.addAttribute("posts", posts);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("pageSize", size);
         return "feed";
     }
 
