26e27e5a782321cec98cdf9811376f1e27fe6368
Roman Orinichev <roma-orinich@yandex.ru>
2025-02-01 17:44:43 +0100

    Added migration outline

---
+++ src/main/java/ru/practicum/java/blog_app_spring_boot_project/BlogAppSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.java.blog_app_spring_boot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppSpringBootProjectApplication.class, args);
+	}
+
+}
