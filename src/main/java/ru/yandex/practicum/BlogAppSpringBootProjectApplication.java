1b46bad5eca7d87af9163702337706fe1d76554b
Roman Orinichev <roma-orinich@yandex.ru>
2025-02-01 22:29:36 +0100

    Switched to Spring Boot from pure Spring: changed dependencies, removed unnecessary configs

---
+++ src/main/java/ru/yandex/practicum/BlogAppSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppSpringBootProjectApplication.class, args);
+	}
+
+}
