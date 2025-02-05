69598f730ff2a4c9cff44d2cff8ab4071c30808b
Roman Orinichev <roma-orinich@yandex.ru>
2025-02-05 02:14:02 +0100

    Added MVC tests

--- src/test/java/ru/yandex/practicum/controller/PostControllerIntegrationTest.java
+++ src/test/java/ru/yandex/practicum/controller/PostControllerIntegrationTest.java
@@ -1,63 +1,45 @@
 package ru.yandex.practicum.controller;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import ru.yandex.practicum.config.MockMvcConfiguration;
-import ru.yandex.practicum.config.WebConfiguration;
 import ru.yandex.practicum.model.Post;
-import ru.yandex.practicum.service.CommentService;
 import ru.yandex.practicum.service.PostService;
-import ru.yandex.practicum.service.TagService;
 
-import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
 
-import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringJUnitConfig(classes = {WebConfiguration.class, MockMvcConfiguration.class})
-@WebAppConfiguration
-@TestPropertySource(locations = "classpath:application-mvc-test.properties")
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("classpath:application-mvc-test.properties")
 public class PostControllerIntegrationTest {
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
 
     @Autowired
     private PostService postService;
 
     @Autowired
-    private TagService tagService;
-
-    @Autowired
-    private CommentService commentService;
-
     private MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-    }
 
     @Test
     void testGetPostById_successful() throws Exception {
         Post post = new Post("Post Title", "Post Content", "url", 1);
 
-        when(postService.getPostById(1L)).thenReturn(post);
+        postService.addPost(post, Collections.emptyList());
+        long lastPostId = postService.getPosts(1, 50).stream().sorted(Comparator.comparing(Post::getId)).toList().getLast().getId();
 
-        mockMvc.perform(get("/post/1"))
+        mockMvc.perform(get("/post/" + lastPostId))
                 .andExpect(status().isOk())
                 .andExpect(view().name("post"))
                 .andExpect(model().attributeExists("post"))
-                .andExpect(model().attribute("post", post));
+                .andExpect(model().attribute("post", hasProperty("title", is("Post Title"))));
     }
 
     @Test
@@ -69,21 +51,17 @@ public class PostControllerIntegrationTest {
                         .param("tags", "Tag1,Tag2"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/feed"));
-
-        verify(postService, times(1)).addPost(any(Post.class), eq(List.of("Tag1", "Tag2")));
     }
 
     @Test
     void testEditPost_shouldRedirectToPost() throws Exception {
-        mockMvc.perform(post("/post/1/edit")
-                        .param("title", "Updated Title")
-                        .param("text", "Updated Content")
+        mockMvc.perform(post("/post/5/edit")
+                        .param("title", "Some New Updated Title")
+                        .param("text", "Some New Updated Content")
                         .param("imageUrl", "url")
-                        .param("tags", "UpdatedTag"))
+                        .param("tags", "SomeNewUpdatedTag"))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/post/1"));
-
-        verify(postService, times(1)).updatePost(any(Post.class), eq(List.of("UpdatedTag")));
+                .andExpect(redirectedUrl("/post/5"));
     }
 
     @Test
@@ -91,8 +69,6 @@ public class PostControllerIntegrationTest {
         mockMvc.perform(post("/post/1/delete"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/feed"));
-
-        verify(postService, times(1)).deletePost(1L);
     }
 
     @Test
@@ -100,8 +76,6 @@ public class PostControllerIntegrationTest {
         mockMvc.perform(post("/post/1/like"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/post/1"));
-
-        verify(postService, times(1)).incrementLikeCount(1L);
     }
 
 }
